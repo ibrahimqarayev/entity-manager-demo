2f44e2780d112c3ab6ddfe7fd4d6b317839d84bb
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-16 22:33:15 +0400

    Initial commit

---
+++ src/test/java/com/ibrahim/entitymanagerdemo/EntityManagerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.entitymanagerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
