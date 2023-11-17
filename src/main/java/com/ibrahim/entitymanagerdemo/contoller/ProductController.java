999fc3d065d2eebf4f7d2babbb20c1ec33261b46
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-17 09:24:21 +0300

    Create CategoryController.java

--- src/main/java/com/ibrahim/entitymanagerdemo/contoller/ProductController.java
+++ src/main/java/com/ibrahim/entitymanagerdemo/contoller/ProductController.java
@@ -1,4 +1,31 @@
 package com.ibrahim.entitymanagerdemo.contoller;
 
+import com.ibrahim.entitymanagerdemo.domain.Product;
+import com.ibrahim.entitymanagerdemo.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable int id) {
+        return productService.findById(id);
+    }
+
+    @GetMapping("/{name}")
+    public Product getByName(@PathVariable String name) {
+        return productService.findByName(name);
+    }
 }
