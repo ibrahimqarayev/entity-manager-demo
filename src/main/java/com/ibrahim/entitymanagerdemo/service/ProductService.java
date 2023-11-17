5dc1692e1b3bc4a98962b0bb814ab1670f783efd
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-17 09:18:20 +0300

    Create ProductService.java

--- src/main/java/com/ibrahim/entitymanagerdemo/service/ProductService.java
+++ src/main/java/com/ibrahim/entitymanagerdemo/service/ProductService.java
@@ -1,4 +1,28 @@
 package com.ibrahim.entitymanagerdemo.service;
 
+import com.ibrahim.entitymanagerdemo.domain.Product;
+import com.ibrahim.entitymanagerdemo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(int id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    public Product findByName(String name) {
+        return productRepository.findByName(name).orElseThrow();
+    }
+
 }
