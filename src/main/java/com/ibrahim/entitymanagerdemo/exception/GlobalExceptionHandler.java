043e7d8fe30dc8f86171c134a7b52bf459edbabf
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-18 00:28:24 +0400

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/ibrahim/entitymanagerdemo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.ibrahim.entitymanagerdemo.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
