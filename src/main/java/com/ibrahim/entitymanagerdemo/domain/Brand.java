3c7df6784db9a3e54500fb31054142327f4a3e6e
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-18 00:31:05 +0400

    Create Brand.java

---
+++ src/main/java/com/ibrahim/entitymanagerdemo/domain/Brand.java
@@ -0,0 +1,20 @@
+package com.ibrahim.entitymanagerdemo.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "brands")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    private String name;
+    private String description;
+}
