fc2c56e186d359b151bc5b5d3e024e1f55149de7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-19 23:07:17 +0300

    Update

--- src/main/java/com/ibrahim/entitymanagerdemo/service/CategoryService.java
+++ src/main/java/com/ibrahim/entitymanagerdemo/service/CategoryService.java
@@ -1,12 +1,28 @@
 package com.ibrahim.entitymanagerdemo.service;
 
+import com.ibrahim.entitymanagerdemo.domain.Category;
+import com.ibrahim.entitymanagerdemo.dto.response.CategoryResponse;
+import com.ibrahim.entitymanagerdemo.exception.ResourceNotFoundException;
 import com.ibrahim.entitymanagerdemo.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final DtoConversionService conversionService;
+
+    public List<CategoryResponse> findAll() {
+        List<Category> categories = categoryRepository.findAll();
+        return conversionService.toCategoryResponseList(categories);
+    }
+
+    protected Category findByName(String name) {
+        return categoryRepository.findByName(name)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found with name: " + name));
+    }
 
 }
