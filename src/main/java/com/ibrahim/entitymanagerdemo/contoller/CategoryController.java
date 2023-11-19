b786e27d4ae069025de68f873be5a6ed3332a6ff
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-19 23:08:02 +0300

    Update

--- src/main/java/com/ibrahim/entitymanagerdemo/contoller/CategoryController.java
+++ src/main/java/com/ibrahim/entitymanagerdemo/contoller/CategoryController.java
@@ -1,4 +1,22 @@
 package com.ibrahim.entitymanagerdemo.contoller;
 
+import com.ibrahim.entitymanagerdemo.dto.response.CategoryResponse;
+import com.ibrahim.entitymanagerdemo.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryResponse> findAll() {
+        return categoryService.findAll();
+    }
 }
