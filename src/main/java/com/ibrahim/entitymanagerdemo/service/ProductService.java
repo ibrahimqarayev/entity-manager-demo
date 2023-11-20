f9a32e24ec726936afce9de7bcf9f8a77b48c0aa
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-20 23:14:24 +0400

    Update

--- src/main/java/com/ibrahim/entitymanagerdemo/service/ProductService.java
+++ src/main/java/com/ibrahim/entitymanagerdemo/service/ProductService.java
@@ -6,10 +6,12 @@ import com.ibrahim.entitymanagerdemo.domain.Product;
 import com.ibrahim.entitymanagerdemo.dto.request.CreateProductRequest;
 import com.ibrahim.entitymanagerdemo.dto.request.UpdateProductRequest;
 import com.ibrahim.entitymanagerdemo.dto.response.ProductResponse;
+import com.ibrahim.entitymanagerdemo.exception.ResourceNotFoundException;
 import com.ibrahim.entitymanagerdemo.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,6 +25,24 @@ public class ProductService {
     public List<ProductResponse> findAll() {
         List<Product> products = productRepository.findAll();
         return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findByCategory(String categoryName) {
+        List<Product> products = productRepository.findByCategory(categoryName);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findByBrand(String brandName) {
+        List<Product> products = productRepository.findByBrand(brandName);
+        return conversionService.toProductResponseList(products);
+    }
+
+    protected Product findByName(String name) {
+        return productRepository.findByName(name).orElseThrow();
+    }
+
+    protected Product findById(int id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found with id :" + id));
     }
 
     public ProductResponse create(CreateProductRequest createRequest) {
@@ -46,6 +66,7 @@ public class ProductService {
         product.setDescription(updateRequest.getDescription());
         product.setCategory(category);
         product.setBrand(brand);
+
         Product updated = productRepository.update(product);
         return conversionService.toProductResponse(updated);
     }
@@ -60,12 +81,63 @@ public class ProductService {
         return conversionService.toProductResponse(saved);
     }
 
-    protected Product findById(int id) {
-        return productRepository.findById(id).orElseThrow();
+    public List<ProductResponse> findByNameLike(String name) {
+        List<Product> products = productRepository.findByNameLike(name);
+        return conversionService.toProductResponseList(products);
     }
 
-    protected Product findByName(String name) {
-        return productRepository.findByName(name).orElseThrow();
+    public List<ProductResponse> findProductsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice) {
+        List<Product> products = productRepository.findProductsByPriceRange(minPrice, maxPrice);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findProductsByNameStartsWithAndCategory(String prefix, String categoryName) {
+        List<Product> products = productRepository.findProductsByNameStartsWithAndCategory(prefix, categoryName);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findProductsByNameStartsWithAndCategoryAndBrand(String prefix, String categoryName, String brandName) {
+        List<Product> products = productRepository.findProductsByNameStartsWithAndCategoryAndBrand(prefix, categoryName, brandName);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public int countAllProducts() {
+        return productRepository.countAllProducts();
+    }
+
+    public List<ProductResponse> sortByPriceHighToLow() {
+        List<Product> products = productRepository.sortByPriceHighToLow();
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> sortByPriceLowToHigh() {
+        List<Product> products = productRepository.sortByPriceLowToHigh();
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> sortByStockQuantityToHigh() {
+        List<Product> products = productRepository.sortByStockQuantityToHigh();
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> sortByStockQuantityToLow() {
+        List<Product> products = productRepository.sortByStockQuantityToLow();
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findProductsByCategoryAndBrand(String categoryName, String brandName) {
+        List<Product> products = productRepository.findProductsByCategoryAndBrand(categoryName, brandName);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findProductsByCategoryAndPriceRange(String categoryName, BigDecimal minPrice, BigDecimal maxPrice) {
+        List<Product> products = productRepository.findProductsByCategoryAndPriceRange(categoryName, minPrice, maxPrice);
+        return conversionService.toProductResponseList(products);
+    }
+
+    public List<ProductResponse> findProductsByCategoryAndBrandAndPriceRange(String categoryName, String brandName, BigDecimal minPrice, BigDecimal maxPrice) {
+        List<Product> products = productRepository.findProductsByCategoryAndBrandAndPriceRange(categoryName, brandName, minPrice, maxPrice);
+        return conversionService.toProductResponseList(products);
     }
 
 }
